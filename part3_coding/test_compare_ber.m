3b2f1b817d4b907bfed4c78362f443af6264289f
Diaa-Essam <studywithdod@gmail.com>
2026-05-09 13:09:59 +0200

    feat: compare BER performance for multiple repetiton factors

--- part3_coding/test_compare_ber.m
+++ part3_coding/test_compare_ber.m
@@ -3,42 +3,54 @@ clear;
 close all;
 
 N = 1000;
-L = 3;
+
+L_values = [3 5 7];
 p_values = 0:0.05:0.5;
 
 ber_uncoded = zeros(size(p_values));
-ber_coded = zeros(size(p_values));
+ber_coded = zeros(length(L_values), length(p_values));
 
 for k = 1:length(p_values)
+
     p = p_values(k);
-    
+
     bits = generate_bits(N);
 
+    % Uncoded
     received_uncoded = bsc_channel(bits, p);
     ber_uncoded(k) = compute_ber(bits, received_uncoded);
 
-    coded = repetition_encode(bits, L);
-    received_coded = bsc_channel(coded, p);
-    decoded = repetition_decode(received_coded, L);
-    ber_coded(k) = compute_ber(bits, decoded);
-end 
+    % Coded
+    for j = 1:length(L_values)
 
+        L = L_values(j);
+
+        coded = repetition_encode(bits, L);
+
+        received_coded = bsc_channel(coded, p);
+
+        decoded = repetition_decode(received_coded, L);
+
+        ber_coded(j, k) = compute_ber(bits, decoded);
+
+    end
+end
+
+% Plot
 figure;
-plot(p_values, ber_uncoded, 'o-', 'LineWidth', 2); hold on;
-plot(p_values, ber_coded, 's-', 'LineWidth', 2);
-legend('Uncoded', 'Repetition Code(L = 3)');
-xlabel('Channel Error Probability(p)');
+
+plot(p_values, ber_uncoded, 'LineWidth', 2);
+hold on;
+
+plot(p_values, ber_coded(1,:), 'LineWidth', 2);
+plot(p_values, ber_coded(2,:), 'LineWidth', 2);
+plot(p_values, ber_coded(3,:), 'LineWidth', 2);
+
+legend('Uncoded', 'L = 3', 'L = 5', 'L = 7');
+
+xlabel('Channel Error Probability (p)');
 ylabel('BER');
-title('BER Comparison');
+
+title('BER Comparison for Different Repetition Factors');
+
 grid on;
-
-
-grid on;
-
-% Observation:
-% Repetition coding reduces BER compared to uncoded transmission.
-% The improvement happens because each bit is transmitted multiple times,
-% allowing the receiver to correct some random channel errors using majority voting.
-%
-% At high channel error probabilities, both systems degrade,
-% since too many bit errors occur.
